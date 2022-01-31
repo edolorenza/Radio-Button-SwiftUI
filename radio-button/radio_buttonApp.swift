3a472bf1b8e5c168863a20911af10afa5a57493f
edo Lorenza <edolrnz@gmail.com>
2022-01-31 16:41:27 +0700

    finish radio button

---
+++ radio-button/radio_buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  radio_buttonApp.swift
+//  radio-button
+//
+//  Created by Edo Lorenza on 31/01/22.
+//
+
+import SwiftUI
+
+@main
+struct radio_buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
